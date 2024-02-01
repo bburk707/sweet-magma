548afb489f442839807fe91abd6d3ccba6a0ae0a
bburkett7 <bburkett7@gatech.edu>
2024-01-31 20:07:15 -0500

    Added build.yaml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
 
-export default function Home() {
+
+export default function Page() {
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1>Hello, Next.js!</h1>
+          <button
+            className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+            Save changes
+          </button>
+        </div>
+      </main>
+  );
+}
+export function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
